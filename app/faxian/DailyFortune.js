263a941ee2ba726d2697195379265dc32c8bbcfa
lovecross <lovecrossyoutt@hotmail.com>
2017-03-09 18:02:59 +0800

    xx

--- app/faxian/DailyFortune.js
+++ app/faxian/DailyFortune.js
@@ -9,13 +9,26 @@ import {
     View
 } from 'react-native';
 import NavigationBar from 'react-native-navbar'
+import ParallaxView from 'react-native-parallax-view'
+
+
+class Header extends Component{
+    render(){
+        return <View>
+
+        </View>
+    }
+}
+
 
 export default class DailyFortune extends Component{
     render(){
-        return <View style={{flex:1,backgroundColor:'#f5f5f5'}}>
-            <NavigationBar
-                title={{title:'运程'}}
-                tintColor="#f7f7f8"/>
-        </View>
+        return <ParallaxView
+            style={{flex:1,backgroundColor:'#f5f5f5'}}
+            backgroundSource={require('../../img/me/betting-record_bg.png')}
+            windowHeight={200}
+            header={<Header />}
+        >
+        </ParallaxView>
     }
 }
